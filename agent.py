a827f40d562a3f002c1b345037443854909711ef
Patrick Cotter <patrick.xalo@gmail.com>
2025-09-22 09:16:23 -0400

    Fix: wrap SHEET_ID in quotes

--- agent.py
+++ agent.py
@@ -5,6 +5,6 @@ scope = ["https://www.googleapis.com/auth/spreadsheets"]
 creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
 client = gspread.authorize(creds)
 
-sheet = client.open_by_key(1AbCdEfGhIjKlMnOpQrStUvWxYz1234567890
+sheet = client.open_by_key("1AbCdEfGhIjKlMnOpQrStUvWxYz1234567890"
 ).sheet1
 
